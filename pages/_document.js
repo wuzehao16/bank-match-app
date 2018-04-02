33d500ae8f02a0917d0e6688c7004909103de963
aisi <wuzehao16@qq.com>
2018-04-02 17:16:06 +0800

    add next css

--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,7 @@ export default class MyDocument extends Document {
           />
           {/* PWA primary color */}
           {/* <meta name="theme-color" content={pageContext.theme.palette.primary.main} /> */}
+          <link rel="stylesheet" href="/_next/static/style.css" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
