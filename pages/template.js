07d7de263afa67a5f49ad53d465651d5d4b9581c
aisi <wuzehao16@qq.com>
2018-04-13 14:08:59 +0800

    layout to layout

--- pages/template.js
+++ pages/template.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
-import Layout from '../components/layout';
+import Layout from '../layout/layout';
 
 class ProductDetail extends React.PureComponent {
   render() {
