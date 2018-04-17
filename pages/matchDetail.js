de3a008b6a189191ad6a3faa853f78c96fad534a
aisi <wuzehao16@qq.com>
2018-04-17 21:55:37 +0800

    finish matchDetail

--- pages/matchDetail.js
+++ pages/matchDetail.js
@@ -65,6 +65,10 @@ const Divide = styled.div`
   background: #f2f2f2;
   margin: 10px 0;
 `
+const SubTitle = styled.div`
+  color: #646464;
+  font-size: 14px;
+`
 const loanType = ['信用贷款', '抵押贷款'];
 
 const city = ['深圳', '广州', '东莞', '珠海', '惠州', '中山'];
@@ -192,6 +196,53 @@ class MatchDetail extends React.PureComponent {
     const assets = i.assets|| {};
     //资产负债
     const capitalDebtSituation = i.capitalDebtSituation|| {};
+    // const house = assets.housePropertyDependency.length;
+    var h  = assets.housePropertyDependency || []
+    const House = h.map((k,index) => {
+      return (
+        <div key={index}>
+          <SubTitle>房产{index+1}:</SubTitle>
+          <Li><L>名下房产属地：</L><R>{housePropertyDependency[assets.housePropertyDependency[index]]}</R></Li>
+          <Li><L>产权归属：</L><R>{ownership[assets.ownership[index]]}</R></Li>
+          <Li><L>产权占比（%）：</L><R>{assets.propertyRightRatio[index]}</R></Li>
+          <Li><L>房产类型：</L><R>{propertyType[assets.propertyType[index]]}</R></Li>
+          <Li><L>房产状态：</L><R>{propertyStatus[assets.propertyStatus[index]]}</R></Li>
+          <Li><L>还款方式：</L><R>{repaymentMethod[assets.repaymentMethod[index]]}</R></Li>
+          <Li><L>本笔抵押贷款余额（元）：</L><R>{assets.mortgageBalance[index]}</R></Li>
+          <Li><L>每月还款金额（元）</L><R>{assets.mortgageMonthRepayment[index]}</R></Li>
+          <Li><L>本笔抵押贷款期限（月）：</L><R>{assets.mortgageMonthRepayment[index]}</R></Li>
+          <Li><L>已还月份数（月）：</L><R>{assets.returnedMonths[index]}</R></Li>
+          <Li><L>本房产总面积（平）：</L><R>{assets.propertyTotalArea[index]}</R></Li>
+          <Li><L>本房产总市值（万元）：</L><R>{assets.propertyTotalMarketValue[index]}</R></Li>
+        </div>
+      )
+    })
+    var c = assets.carRegistStatus || []
+    const Car = c.map((k,index) => {
+      return (
+        <div key={index}>
+          <SubTitle>辆车{index+1}:</SubTitle>
+          <Li><L>车辆登记状态：</L><R>{carRegistStatus[assets.carRegistStatus[index]]}</R></Li>
+          <Li><L>车辆贷款余额：</L><R>{assets.carLoanBalance?assets.carLoanBalance[index]:''}</R></Li>
+          <Li><L>车辆残值评估价格（元）：</L><R>{assets.carPotentialPrice?assets.carPotentialPrice[index]:''}</R></Li>
+          <Li><L>车贷还款金额（元）：</L><R>{assets.carRepayment?assets.carRepayment[index]:''}</R></Li>
+          <Li><L>车贷还款月份（月）：</L><R>{assets.carrRepaymentMonths?assets.carrRepaymentMonths[index]:''}</R></Li>
+        </div>
+      )
+    })
+    var p = assets.policyBrand || []
+    const policy = p.map((k,index) => {
+      return (
+        <div key={index}>
+          <SubTitle>保单{index+1}:</SubTitle>
+          <Li><L>保单品牌：</L><R>{policyBrand[assets.policyBrand[index]]}</R></Li>
+          <Li><L>其他保单品牌：</L><R>{assets.otherPolicyBrand?assets.otherPolicyBrand[index]:''}</R></Li>
+          <Li><L>保单缴纳方式：</L><R>{policyBrandPaymentMethod[assets.policyBrandPaymentMethod[index]]}</R></Li>
+          <Li><L>保单缴费时长：</L><R>{policyPaymentYears[assets.policyPaymentYears[index]]}</R></Li>
+          <Li><L>保单缴费金额（元）：</L><R>{assets.policyPayment?assets.policyPayment[index]:''}</R></Li>
+        </div>
+      )
+    })
     return (
       <Layout>
         <Wrapper>
@@ -205,7 +256,7 @@ class MatchDetail extends React.PureComponent {
           </Base>
           <More>
             <span><i className="daikuan"></i><span>期望贷款：{loanDemand.exLoanAmount}万</span></span>
-            <span><i className="leixing"></i><span>贷款类型：{loanDemand.loanType}</span></span>
+            <span><i className="leixing"></i><span>贷款类型：{loanType[loanDemand.loanType]}</span></span>
           </More>
         </Wrapper>
         <Wrapper>
@@ -318,7 +369,7 @@ class MatchDetail extends React.PureComponent {
               ?<div>
                 <Li><L>名下房产数量（套）：</L><R>{assets.sumHouseProperty}</R></Li>
                 {/* 循环 */}
-                <Li><L>名下房产属地：</L><R>{housePropertyDependency[assets.housePropertyDependency]}</R></Li>
+                {/* <Li><L>名下房产属地：</L><R>{housePropertyDependency[assets.housePropertyDependency]}</R></Li>
                 <Li><L>产权归属：</L><R>{ownership[assets.ownership]}</R></Li>
                 <Li><L>产权占比（%）：</L><R>{assets.propertyRightRatio}</R></Li>
                 <Li><L>房产类型：</L><R>{propertyType[assets.propertyType]}</R></Li>
@@ -329,26 +380,31 @@ class MatchDetail extends React.PureComponent {
                 <Li><L>本笔抵押贷款期限（月）：</L><R>{assets.mortgageMonthRepayment}</R></Li>
                 <Li><L>已还月份数（月）：</L><R>{assets.returnedMonths}</R></Li>
                 <Li><L>本房产总面积（平）：</L><R>{assets.propertyTotalArea}</R></Li>
-                <Li><L>本房产总市值（万元）：</L><R>{assets.propertyTotalMarketValue}</R></Li>
-
+                <Li><L>本房产总市值（万元）：</L><R>{assets.propertyTotalMarketValue}</R></Li> */}
+                {House}
+                <Divide></Divide>
               </div> :null
           }
 
-
-
-
-
-
-
-
-
-
-
-
-
           <Li><L>名下商业保单：</L><R>{businessPolicy[assets.businessPolicy]}</R></Li>
+          {
+            assets.businessPolicy == 1
+              ?<div>
+                <Li><L>保单份数（份）：</L><R>{assets.sumHouseProperty}</R></Li>
+                {/* 循环 */}
+                {policy}
+                <Divide></Divide>
+              </div> :null
+          }
           <Li><L>名下是否有家用汽车：</L><R>{isFamilyCar[assets.isFamilyCar]}</R></Li>
-
+          {
+            assets.isHouseProperty == 1
+              ?<div>
+                <Li><L>名下车辆数（辆）：</L><R>{assets.sumHouseProperty}</R></Li>
+                {/* 循环 */}
+                {Car}
+              </div> :null
+          }
         </Wrapper>
         <Wrapper>
           <Title>资产负债</Title>
