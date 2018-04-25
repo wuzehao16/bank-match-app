68d76c72364f6efc5aebd5d0c7dc1458ccb26b79
aisi <wuzehao16@qq.com>
2018-04-25 19:21:36 +0800

    icon 字体调整

--- pages/help.js
+++ pages/help.js
@@ -5,18 +5,23 @@ import styled from 'styled-components'
 import Layout from '../layout/Blanklayout'
 import Icon from 'material-ui/Icon'
 
-const iconStyles = {
-  color: '#c4c4c4',
-  fontSize:'13px',
-  position: 'absolute',
-  right: '45px',
-  lineHeight: '45px'
-}
 const ulStyles = {
   listStyle: 'none',
   width:'100%',
   marginTop:'15px',
 }
+const Li = styled.li`
+  font-size:13px;
+  height: 45px;
+  color: #646464;
+  line-height: 45px;
+  position: relative;
+  border-bottom: 1px solid #f2f2f2;
+  padding: 0 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
 
 class Help extends React.PureComponent {
   render() {
@@ -25,22 +30,23 @@ class Help extends React.PureComponent {
       <div className="helplist">
         <ul style={ulStyles}>
           <Link href='/helpquestions'>
-            <li>
-              常见问题
-              <i class="material-icons" style={iconStyles}>chevron right</i>
-            </li>
+            <Li>
+              <span>常见问题</span>
+
+              <Icon>chevron_right</Icon>
+            </Li>
           </Link>
           <Link href='/contactus'>
-            <li>
-            联系客服
-            <i class="material-icons" style={iconStyles}>chevron right</i>
-            </li>
+            <Li>
+            <span>联系客服</span>
+            <Icon>chevron_right</Icon>
+            </Li>
           </Link>
         </ul>
-      </div>  
+      </div>
         <style jsx global>{`
           html, body, div, span,
-          h1, h2, h3, h4, h5, h6, p, 
+          h1, h2, h3, h4, h5, h6, p,
           a,footer, header, nav{
             margin: 0;
             padding: 0;
@@ -72,17 +78,6 @@ class Help extends React.PureComponent {
           .helplist {
             width: 100%;
             background: #fff;
-
-          }
-          ul li {
-            width:100%;
-            font-size:13px;
-            height: 45px;
-            color: #646464;
-            line-height: 45px;
-            position: relative;
-            border-bottom: 1px solid #f2f2f2;
-            padding: 0 16px;
 
           }
           ul li:last-child {
