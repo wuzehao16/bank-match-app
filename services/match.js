dc8ad7514321174cc0c4c9275e1d7169f4012de6
aisi <wuzehao16@qq.com>
2018-04-28 18:16:32 +0800

    change 请求url

--- services/match.js
+++ services/match.js
@@ -5,7 +5,7 @@ import request from '../lib/request';
 //   return request(`/sys/selectModelList?${stringify(params)}`);
 // }
 export async function queryDetail(params) {
-  return request(`http://192.168.2.115:8611/app/selectModelUserDetail?matchNo=${params}`);
+  return request(`https://api.ibankmatch.com:8620/app/selectModelUserDetail?matchNo=${params}`);
 }
 
 export async function update(params) {
@@ -18,7 +18,7 @@ export async function update(params) {
 }
 
 export async function add(params) {
-  return request('http://47.106.70.82:8611/app/getMatchModeResult', {
+  return request('https://api.ibankmatch.com:8620/app/getMatchModeResult', {
    // return request('http://192.168.2.100:8611/app/getMatchModeResult', {
     method: 'POST',
     body: {
