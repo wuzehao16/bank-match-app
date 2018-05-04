88db841e0883b0d4e9576a2d7612b76b4c30194e
aisi <wuzehao16@qq.com>
2018-05-04 16:25:20 +0800

    change url to 测试

--- services/match.js
+++ services/match.js
@@ -5,7 +5,7 @@ import request from '../lib/request';
 //   return request(`/sys/selectModelList?${stringify(params)}`);
 // }
 export async function queryDetail(params) {
-  return request(`https://api.ibankmatch.com:8620/app/selectModelUserDetail?matchNo=${params}`);
+  return request(`http://ad.ibankmatch.com:8611/app/selectModelUserDetail?matchNo=${params}`);
 }
 
 export async function update(params) {
@@ -18,7 +18,7 @@ export async function update(params) {
 }
 
 export async function add(params) {
-  return request('https://api.ibankmatch.com:8620/app/getMatchModeResult', {
+  return request('http://ad.ibankmatch.com:8611/app/getMatchModeResult', {
    // return request('http://192.168.2.100:8611/app/getMatchModeResult', {
     method: 'POST',
     body: {
