065d2b405909b511732716599241e7f162d57b15
aisi <wuzehao16@qq.com>
2018-05-10 17:53:44 +0800

    download 样式修改

--- pages/download.js
+++ pages/download.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import styled from 'styled-components'
 import Button from 'material-ui/Button';
 import { withStyles } from 'material-ui/styles';
+import Divider from 'material-ui/Divider';
 import Layout from '../layout/Blanklayout';
 import withRoot from '../src/withRoot'
 
@@ -11,8 +12,9 @@ const styles = theme => ({
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     borderRadius: 25,
     border: 0,
+    width: 200,
     color: 'white',
-    height: 30,
+    height: 48,
     padding: '0 30px',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
   },
@@ -33,10 +35,12 @@ class Download extends React.PureComponent {
     return (
       <Layout>
         <Wrapper>
-          <img src="/static/logo.png" style={{width:100,height:100,display:'block',margin:'60px auto 15px'}}/>
-          <h1 style={{color:'#646464',fontSize:'13px',textAlign:'center'}}>众银云测</h1>
-          <h2 style={{color:'#646464',fontSize:'9px',textAlign:'center',marginTop:5}}>V1.0.0</h2>
-          <a style={{  textDecoration: 'none',display:'block',width:'100px',margin:'150px auto'}} href="itms-services://?action=download-manifest&amp;url=https://back.ibankmatch.com/manifest.plist">
+          <img src="/static/logo.png" style={{width:120,height:120,display:'block',margin:'60px auto 15px'}}/>
+          <h1 style={{color:'#646464',fontSize:'24px',textAlign:'center',marginTop:'50px'}}>众银云测</h1>
+          <Divider style={{marginTop:'30px'}}/>
+          <h2 style={{color:'#646464',fontSize:'16px',textAlign:'center',marginTop:30}}>V1.0.0</h2>
+
+          <a style={{  textDecoration: 'none',display:'block',width:'200px',margin:'30px auto'}} href="itms-services://?action=download-manifest&amp;url=https://back.ibankmatch.com/manifest.plist">
           <Button className={classes.button}>下载</Button>
           </a>
         </Wrapper>
