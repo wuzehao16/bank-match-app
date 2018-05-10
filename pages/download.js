31ad75392f288b2f0d5da627c4474d7abffa0ef9
aisi <wuzehao16@qq.com>
2018-05-10 17:42:04 +0800

    download button圆角

--- pages/download.js
+++ pages/download.js
@@ -9,10 +9,10 @@ import withRoot from '../src/withRoot'
 const styles = theme => ({
   button: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 3,
+    borderRadius: 25,
     border: 0,
     color: 'white',
-    height: 48,
+    height: 30,
     padding: '0 30px',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
   },
