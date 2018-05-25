bf9fa88906bf3092c1ea5554fc5702bf15afdb0f
LingWuya <478399567@qq.com>
2018-05-25 16:38:51 +0800

    '公司信息'

--- pages/companyInfo.js
+++ pages/companyInfo.js
@@ -16,6 +16,10 @@ const P = styled.p`
 
 class CompanyInfo extends React.PureComponent {
   state = {
+    files: [{
+      url: '',
+      id:''
+    }],
     multiple: false,
   }
 
@@ -27,10 +31,11 @@ class CompanyInfo extends React.PureComponent {
   }
 
   render () {
-    // const { files } = this.state;
+    const { files } = this.state;
     const { getFieldProps, getFieldsError } = this.props.form;
     return (
       <Layout title="公司信息">
+        <WhiteSpace />
         <InputItem
             {...getFieldProps('companyName',{rules:[
               {
@@ -46,16 +51,16 @@ class CompanyInfo extends React.PureComponent {
           // files={files}
           onChange={this.onChange}
           onImageClick={(index, fs) => console.log(index, fs)}
-          selectable={true}
-          multiple={false}
+          // selectable={true}
+          // multiple={false}
         />
         <P>上传营业执照</P>
         <ImagePicker
           // files={files}
           onChange={this.onChange}
           onImageClick={(index, fs) => console.log(index, fs)}
-          selectable={true}
-          multiple={false}
+          // selectable={true}
+          // multiple={false}
         />
         <P>上传公司logo</P>
         <style jsx global>{`
@@ -63,6 +68,7 @@ class CompanyInfo extends React.PureComponent {
           font-size: 14px !important;
         }
         .am-list-item .am-input-control input {
+          font-size: 14px !important;
           color: #888 !important;
           text-align: right;
         }
